940ee2bd36fa51ec09fb8712d1dde5e974c40e30
Ian <gacek004@umn.edu>
2019-05-05 16:12:05 -0700

    html updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){//I need jQuery to run!
 
     var targetScore, current, redNumber, purpleNumber, blueNumber, greenNumber;
@@ -13,14 +12,14 @@ $(document).ready(function(){//I need jQuery to run!
 	    purpleNumber = Math.floor(Math.random()*12);
 	    blueNumber = Math.floor(Math.random()*12);
 	    greenNumber = Math.floor(Math.random()*12);
+        $("#button-red").attr('value', redNumber);
+        $("#button-blue").attr('value', blueNumber);
+        $("#button-purple").attr('value', purpleNumber);
+        $("#button-green").attr('value', greenNumber);
     }
 
     resetGame();
 
-	$("#button-red").attr('value', redNumber);
-    $("#button-blue").attr('value', blueNumber);
-    $("#button-purple").attr('value', purpleNumber);
-    $("#button-green").attr('value', greenNumber);
 
 	$('.crystal').click(function(){
         current += parseInt($(this).attr('value'));
@@ -46,39 +45,4 @@ $(document).ready(function(){//I need jQuery to run!
         console.log(targetScore);
         $("#targetScore").html(targetScore);
 
-=======
-$(document).ready(function(){//I need jQuery to run!
-
-    var targetScore, current, redNumber, purpleNumber, blueNumber, greenNumber;
-
-    var wins = 0
-    var losses = 0
-
-    var resetGame = function(){
-        goal = Math.floor(Math.random() * 101 + 19);
-        current = 0;
-        redNumber = Math.floor(Math.random()*12);
-	    purpleNumber = Math.floor(Math.random()*12);
-	    blueNumber = Math.floor(Math.random()*12);
-	    greenNumber = Math.floor(Math.random()*12);
-    }
-
-    resetGame();
-
-	$("#button-red").attr('value', redNumber);
-    $("#button-blue").attr('value', blueNumber);
-    $("#button-purple").attr('value', purpleNumber);
-    $("#button-green").attr('value', greenNumber);
-
-	$('.crystal').click(function(){
-        current += parseInt($(this).attr('value'));
-        console.log(current);
-
-    });
-
-    var targetScore = Math.floor(Math.random() * 120) + 19;
-        console.log(targetScore);
-        $("#targetScore").html(targetScore);
-
->>>>>>> ec47c1a00f664d19e7933d6e3ab8a5153d2025ca
 });
