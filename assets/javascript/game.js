6018660aeb1da44942cf748b5eb2fe412b71bfdc
Ian <gacek004@umn.edu>
2019-05-05 17:46:37 -0700

    javascript updates again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,28 @@
-$(document).ready(function(){//I need jQuery to run!
+$(document).ready(function () {//I need jQuery to run!
 
     var targetScore, current, redNumber, purpleNumber, blueNumber, greenNumber;
 
     var wins = 0
     var losses = 0
 
-    var resetGame = function(){
-        goal = Math.floor(Math.random() * 101 + 19);
+    var resetGame = function () {
         current = 0;
-        redNumber = Math.floor((Math.random()*12) + 1);
-	    purpleNumber = Math.floor((Math.random()*12) +1);
-	    blueNumber = Math.floor((Math.random()*12) + 1);
-	    greenNumber = Math.floor((Math.random()*12) + 1);
+        targetScore = Math.floor(Math.random() * 120) + 19;
+        redNumber = Math.floor((Math.random() * 12) + 1);
+        purpleNumber = Math.floor((Math.random() * 12) + 1);
+        blueNumber = Math.floor((Math.random() * 12) + 1);
+        greenNumber = Math.floor((Math.random() * 12) + 1);
         $("#button-red").attr('value', redNumber);
         $("#button-blue").attr('value', blueNumber);
         $("#button-purple").attr('value', purpleNumber);
         $("#button-green").attr('value', greenNumber);
+        $("#targetScore").attr('value', targetScore);
     }
 
     resetGame();
 
 
-	$('.crystal').click(function(){
+    $('.crystal').click(function () {
         current += parseInt($(this).attr('value'));
         console.log(current);
         $("#currentScore").html(current);
@@ -29,23 +30,26 @@ $(document).ready(function(){//I need jQuery to run!
             // Increments wins by 1
             wins++;
             current = 0;
+            resetGame();
             // Adds an additional value to wins on HTML
             $("#wins").html("Wins: " + wins);
-            console.log('win: ', wins)
+            console.log('win: ', wins);
             alert("A winner is you!");
         } else if (current > targetScore) {
             // Increments losses by 1
             losses++;
+            current = 0;
             resetGame();
-            alert("You lose. Good day!");
             // Adds an additional value to losses on HTML
             $("#losses").html("Losses: " + losses);
+            console.log('loss: ', losses);
+            alert("You lose. Good day!");
         }
 
     });
 
     var targetScore = Math.floor(Math.random() * 120) + 19;
-        console.log(targetScore);
-        $("#targetScore").html(targetScore);
+    console.log(targetScore);
+    $("#targetScore").html(targetScore);
 
 });
